86a950dbf89ba812e63c93038502c1c415ea6656
Ben Bolduc <bolduc.10@osu.edu>
2018-12-08 03:16:28 -0500

    Test os.environ

--- lib/vConTACT/vConTACTImpl.py
+++ lib/vConTACT/vConTACTImpl.py
@@ -51,6 +51,7 @@ class vConTACT:
         #BEGIN run_vcontact
         self.callback_url = os.environ['SDK_CALLBACK_URL']
         params['SDK_CALLBACK_URL'] = self.callback_url
+        print(os.environ)
         params['KB_SERVICE_TOKEN'] = os.environ['KB_SERVICE_TOKEN']
         self.workspace_name = params['workspace_name']
 
